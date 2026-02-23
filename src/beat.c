7fe6fcd52b90af588e956028994041df4563f890
Shinsuke Suzuki <shin@wolverion.com>
2026-02-23 14:23:14 +0900

    Changed to 12-hour system
    - changed to 12-hour system
    - const-correctified

--- src/beat.c
+++ src/beat.c
@@ -88,15 +88,12 @@ static const char month[12][4] = {
 	"Dec"
 };
 
-void set_timezone_offset(char tz_offset[])
+void set_timezone_offset(const char tz_offset[])
 {
-	int hour = 0;
-	int min  = 0;
-
 	strncpy(utc_offset_str, tz_offset, sizeof(utc_offset_str));
 
-	hour = (INT_FROM_DIGIT(tz_offset[1]) * 10) + INT_FROM_DIGIT(tz_offset[2]);
-	min  = (INT_FROM_DIGIT(tz_offset[3]) * 10) + INT_FROM_DIGIT(tz_offset[4]);
+	const int hour = (INT_FROM_DIGIT(tz_offset[1]) * 10) + INT_FROM_DIGIT(tz_offset[2]);
+	const int min  = (INT_FROM_DIGIT(tz_offset[3]) * 10) + INT_FROM_DIGIT(tz_offset[4]);
 
 	utc_offset_seconds = hour * 60 * 60 + min * 60;
 	if (tz_offset[0] != '+') {
@@ -109,20 +106,17 @@ void update_background_callback(Layer * layer, GContext * gctx)
 	(void) layer;
 }
 
-time_t calc_unix_seconds(struct tm * tick_time)
+time_t calc_unix_seconds(const struct tm * tick_time)
 {
 	// This function is necessary because mktime() doesn't work (probably
 	// because there's no native timezone support) and just calling time()
 	// gives us no guarantee that the seconds haven't changed since tick_time
 	// was made.
 
-	int years_since_epoch;
-	int days_since_epoch;
-
 	// This uses a naive algorithm for calculating leap years and will
 	// therefore fail in 2100.
-	years_since_epoch = tick_time->tm_year - 70;
-	days_since_epoch  = (years_since_epoch * 365 + (years_since_epoch + 1) / 4 + tick_time->tm_yday);
+	const int years_since_epoch = tick_time->tm_year - 70;
+	const int days_since_epoch  = (years_since_epoch * 365 + (years_since_epoch + 1) / 4 + tick_time->tm_yday);
 	return days_since_epoch * 86400 + tick_time->tm_hour * 3600 + tick_time->tm_min * 60 + tick_time->tm_sec;
 }
 
@@ -131,15 +125,13 @@ uint16_t calc_swatch_beats(time_t unix_seconds)
 	return ((((unsigned) unix_seconds + 3600U) % 86400U) * 1000U) / 86400U;
 }
 
-void display_time(struct tm * tick_time)
+void display_time(const struct tm * tick_time)
 {
 	// Static, because we pass them to the system.
 	static char date_text[] = "MMM/99";
-	static char time_text[] = "99:99:99";
+	static char time_text[] = "99:99am";
 	static char beat_text[] = "@999";
 	static char step_text[] = "000000 STEPS";
-
-	time_t unix_seconds;
 
 	// Date.
 
@@ -149,43 +141,29 @@ void display_time(struct tm * tick_time)
 
 	// Time.
 
-	strftime(time_text, sizeof(time_text), "%H:%M:%S", tick_time);
-	// if (time_text[0] == ' ') {
-	// 	time_text[0] = '0';
-	// }
+	const int hour_12 = (tick_time->tm_hour % 12 == 0) ? 12 : (tick_time->tm_hour % 12);
+	const char * const am_pm = (tick_time->tm_hour >= 12) ? "pm" : "am";
+	snprintf(time_text, sizeof(time_text), "%d:%02d%s", hour_12, tick_time->tm_min, am_pm);
 	text_layer_set_text(text_time_layer, time_text);
 
 	// Unix timestamp.
 
-	unix_seconds = calc_unix_seconds(tick_time) - utc_offset_seconds;
+	const time_t unix_seconds = calc_unix_seconds(tick_time) - utc_offset_seconds;
 
 	// Swatch .beats.
 
 	snprintf(beat_text, sizeof(beat_text), "@%03d", calc_swatch_beats(unix_seconds));
-	// if (beat_text[3] == '\0') {
-	// 	beat_text[4] = '\0';
-	// 	beat_text[3] = beat_text[2];
-	// 	beat_text[2] = beat_text[1];
-	// 	beat_text[1] = '0';
-	// } else if (beat_text[2] == '\0') {
-	// 	beat_text[4] = '\0';
-	// 	beat_text[3] = beat_text[1];
-	// 	beat_text[2] = '\0';
-	// 	beat_text[1] = '0';
-	// } else {
-	// 	// do nothing
-	// }
 	text_layer_set_text(text_beat_layer, beat_text);
 
 	// Steps
-	uint16_t steps = health_service_sum_today(HealthMetricStepCount);
-	snprintf(step_text, sizeof(step_text), "%5d STEPS", steps);
+	const uint16_t steps = health_service_sum_today(HealthMetricStepCount);
+	snprintf(step_text, sizeof(step_text), "%d STEPS", steps);
 	text_layer_set_text(text_step_layer, step_text);
 }
 
-TextLayer * init_text_layer(GRect rect, GTextAlignment align, uint32_t font_res_id)
+TextLayer * init_text_layer(const GRect rect, const GTextAlignment align, const uint32_t font_res_id)
 {
-	TextLayer * layer = text_layer_create(rect);
+	TextLayer * const layer = text_layer_create(rect);
 	text_layer_set_text_color(layer, TEXT_FG_COLOR);
 	text_layer_set_background_color(layer, TEXT_BG_COLOR);
 	text_layer_set_text_alignment(layer, align);
@@ -196,9 +174,7 @@ TextLayer * init_text_layer(GRect rect, GTextAlignment align, uint32_t font_res_
 
 static void store_timezone()
 {
-	int result;
-
-	result = persist_write_string(1, utc_offset_str);
+	const int result = persist_write_string(1, utc_offset_str);
 	if (result < 0) {
 		APP_LOG(APP_LOG_LEVEL_WARNING, "Storing timezone failed: %d", result);
 	} else {
@@ -244,7 +220,7 @@ static void request_timezone(void)
 
 void in_received_handler(DictionaryIterator * received, void * context)
 {
-	Tuple * utc_offset_tuple = dict_find(received, BEAPOCH_KEY_UTC_OFFSET);
+	const Tuple * utc_offset_tuple = dict_find(received, BEAPOCH_KEY_UTC_OFFSET);
 
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Received timezone: %s", utc_offset_tuple->value->cstring);
 
@@ -278,7 +254,7 @@ static void handle_second_tick(struct tm * tick_time, TimeUnits units_changed)
 
 static void window_load(Window * window)
 {
-	Layer * window_layer = window_get_root_layer(window);
+	Layer * const window_layer = window_get_root_layer(window);
 
 	window_set_background_color(window, WINDOW_COLOR);
 	background_layer = layer_create(layer_get_bounds(window_layer));
@@ -291,7 +267,7 @@ static void window_load(Window * window)
 	text_step_layer = init_text_layer(STEP_RECT, GTextAlignmentCenter, RESOURCE_ID_FONT_STEP_18);
 
 	get_stored_timezone();
-	time_t now = time(NULL);
+	const time_t now = time(NULL);
 	display_time(localtime(&now));
 	tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
 }
